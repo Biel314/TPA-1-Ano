c3f537f013d5dc7b78b5d14fc2b995f3d0573d2d
Gabriel Ordonho <89818304+Biel314@users.noreply.github.com>
2021-11-21 18:37:12 -0300

    Delete Testes directory

--- Testes/aaaaa.java
+++
@@ -1,8 +0,0 @@
-package aaaa;
-
-public class aaaaa {
-	public static void main(String args[]) {
-		
-		System.out.print("\npublic class Nome {\n\tpublic static void main(String args []) {\n\n\t\tSystem.out.print(\"olá\");\n\n\t}\n}");
-	}
-}
